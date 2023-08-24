0a7ad4a6f875346f3188526d4b7dc597c69c27d6
Pavel Kochetkov <kochetkov_p@mail.ru>
2023-08-24 22:38:42 +0300

    code refactoring

--- src/components/PageTitle.tsx
+++ src/components/PageTitle.tsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import '../css/pagetitle.css';
 
 interface DataPropsPageTitle {
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -5,7 +5,7 @@ export const Home = () => {
     return (
         <div className="home">
             <div className="titlehome">Павел Кочетков</div>
-            <div className="jobtitle">Beginner JavaScript developer</div>
+            <div className="jobtitle">Frontend JavaScript developer</div>
         </div>
     );
 };
