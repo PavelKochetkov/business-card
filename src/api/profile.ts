e6014ec4fcd34e96aa2c8336d8e0d6d8928a32b9
Pavel Kochetkov <kochetkov_p@mail.ru>
2023-08-26 00:35:12 +0300

    code refactoring

--- src/api/profile.ts
+++ src/api/profile.ts
@@ -1,24 +1,26 @@
 import axios from "axios";
 import { IDataAbout, IDataContacts, IDataSkill } from "../models/IDataProfile";
+import { TargetJson } from "../models/TargetJson";
+
 
 const baseURL: string = '/business-card/'
 
 export const getProfileContacts = async () => {
-    return axios.get<IDataContacts[]>(`${baseURL}contacts.json`)
+    return axios.get<IDataContacts[]>(`${baseURL}${TargetJson.contacts}`)
 }
 
 export const getProfileAbout = async () => {
-    return axios.get<IDataAbout[]>(`${baseURL}about.json`)
+    return axios.get<IDataAbout[]>(`${baseURL}${TargetJson.about}`)
 }
 
 export const getProfileSkillProgrammingLanguages = async () => {
-    return axios.get<IDataSkill[]>(`${baseURL}programminglanguages.json`) 
+    return axios.get<IDataSkill[]>(`${baseURL}${TargetJson.language}`) 
 }
 
 export const getProfileSkillLibraries = async () => {
-    return axios.get<IDataSkill[]>(`${baseURL}libraries.json`) 
+    return axios.get<IDataSkill[]>(`${baseURL}${TargetJson.libraries}`) 
 }
 
 export const getProfileOtherSkill = async () => {
-    return axios.get<IDataSkill[]>(`${baseURL}other.json`) 
+    return axios.get<IDataSkill[]>(`${baseURL}${TargetJson.other}`) 
 }
---
+++ src/models/TargetJson.ts
@@ -0,0 +1,7 @@
+export enum TargetJson {
+    contacts = 'contacts.json',
+    about = 'about.json',
+    language = 'programminglanguages.json',
+    libraries = 'libraries.json',
+    other = 'other.json'
+}
